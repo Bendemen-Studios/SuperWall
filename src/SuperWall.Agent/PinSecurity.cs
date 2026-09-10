a6c8b35c5506b9397998d644757ddfac6217ebf3
JustPetrov <thepetrovshop@gmail.com>
2026-09-10 15:24:45 +0200

    fix: add hashed PIN support to Kids agent

---
+++ src/SuperWall.Agent/PinSecurity.cs
@@ -0,0 +1,23 @@
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SuperWall.Agent;
+
+public static class PinSecurity
+{
+    public static (string Hash, string Salt) CreateHash(string pin)
+    {
+        var salt = Convert.ToBase64String(RandomNumberGenerator.GetBytes(16));
+        var hash = Convert.ToBase64String(Rfc2898DeriveBytes.Pbkdf2(Encoding.UTF8.GetBytes(pin), Convert.FromBase64String(salt), 120_000, HashAlgorithmName.SHA256, 32));
+        return (hash, salt);
+    }
+    public static bool Verify(string pin, string hash, string salt)
+    {
+        try
+        {
+            var candidate = Rfc2898DeriveBytes.Pbkdf2(Encoding.UTF8.GetBytes(pin), Convert.FromBase64String(salt), 120_000, HashAlgorithmName.SHA256, 32);
+            return CryptographicOperations.FixedTimeEquals(candidate, Convert.FromBase64String(hash));
+        }
+        catch { return false; }
+    }
+}
